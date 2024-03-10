40fec9b2c2580fd6ef46749d47e154f13af63e05
vkuznetsoff <ss_slavich@mail.ru>
2024-03-10 10:55:07 +0300

    fix

--- src/components/SearchFrom/SearchForm.tsx
+++ src/components/SearchFrom/SearchForm.tsx
@@ -1,16 +1,16 @@
 import { useState, useEffect, useContext, useCallback } from "react";
 import "./styles.css";
 import { SearchContext } from "../SearchResults/SearchContext";
-import {useDebounce} from "../../hooks/useDebounce"
+import { useDebounce } from "../../hooks/useDebounce"
 import { SearchContextType, UsersType } from "../../types/types";
 
-const URL = "https://dummyjson.com/users1/search?q=";
+const URL = "https://dummyjson.com/users/search?q=";
 
 
 const SearchForm: React.FC<{}> = () => {
   //Создание стейта для строки запроса
   const [query, setQuery] = useState<String>("");
- 
+
 
   //Получаем данные из контекста
   const { setUsers, setIsLoad, firstLoad } = useContext<SearchContextType>(SearchContext);
@@ -19,16 +19,16 @@ const SearchForm: React.FC<{}> = () => {
   когда прошло не менее 1 секунды от последнего нажатия клавиши в поле поиска
   */
   const debounceQuery = useDebounce(query, 700)
-  
+
   //Асинхронная функция запроса на сервер
-  const fetchUser = async (query: String )  => {
+  const fetchUser = async (query: String) => {
     let response = await fetch(`${URL}${query}`)
 
     if (response.ok) {
-      let result = (await response.json()) 
+      let result = (await response.json())
       setIsLoad(false)
       setUsers(result.users);
-      
+
     } else {
       console.log("Ошибка запроса: " + response.status);
     }
@@ -40,23 +40,32 @@ const SearchForm: React.FC<{}> = () => {
       return;
     }
 
-    fetchUser(debounceQuery)  
+    fetchUser(debounceQuery)
   }, [debounceQuery]);
 
-  return (
-    <div className="searchForm">
-      <form>
-        <input
-          type="text"
-          placeholder="Введите запрос..."
-          value={query}
-          onChange={(e) => {
-            setIsLoad(true)
-            setQuery(e.target.value)}}
-        />
-      </form>
-    </div>
-  );
+  const handleChange = (e) => {
+    
+    setIsLoad(true)
+    setQuery(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  
+return (
+  <div className="searchForm">
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <input
+        type="text"
+        placeholder="Введите запрос..."
+        value={query}
+        onChange={ (e) => handleChange(e) }      
+      />
+    </form>
+  </div>
+);
 }
 
 export default SearchForm
